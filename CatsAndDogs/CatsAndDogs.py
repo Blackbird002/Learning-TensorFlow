69468492b6244aef36b769be299399945fd7cc6c
Riad Shash <riadshash@yahoo.com>
2019-07-03 17:52:15 -0600

    Added Cats and Dogs CNN example script

---
+++ CatsAndDogs/CatsAndDogs.py
@@ -0,0 +1,142 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+#tool for fast prototyping of computer vision problems
+import cv2
+import random
+#binary protocols for serializing and de-serializing a Python object
+import pickle
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
+
+'''
+Location of Dog & Cat Images from Microsoft's Kaggle Cats and Dogs
+dataset:
+https://www.microsoft.com/en-us/download/details.aspx?id=54765
+'''
+dataDir = "C:\\Users\\riads\\Documents\\TFLearning\\PetImages"
+
+'''
+0 - dog
+1 - cat
+'''
+categories = ["Dog", "Cat"]
+imgSize = 80
+
+#Optional (testing)
+def loadImages():
+    for category in categories:
+        #Joins path components intelligently
+        path = os.path.join(dataDir, category)
+        #For every image in the directory...
+        for img in os.listdir(path):
+            imgArr = cv2.imread(os.path.join(path,img),cv2.IMREAD_GRAYSCALE)
+            plt.imshow(imgArr, cmap="gray")
+            plt.show()
+            break
+        break
+        print(imgArr)
+        print(imgArr.shape)
+    return imgArr
+
+#Optional (testing)
+#Need to normalize picture shapes (pictures are different shape)
+def normalizeImgSize(imgArr):
+    resizeArr = cv2.resize(imgArr, (imgSize,imgSize))
+    plt.imshow(resizeArr)
+    plt.show()
+    return resizeArr
+
+def createTrainData(trainData):
+    for category in categories:
+        path = os.path.join(dataDir, category)
+        classNum = categories.index(category)
+        for img in os.listdir(path):
+            #Some images are broken - avoids OS errors and warnings
+            try:
+                imgArr = cv2.imread(os.path.join(path,img),cv2.IMREAD_GRAYSCALE)
+                resizeArr = cv2.resize(imgArr, (imgSize,imgSize))
+                trainData.append([resizeArr, classNum])
+            except Exception as e:
+                pass
+    print("Items in trainData: ", len(trainData))
+    #Shuffle the data so it's not all dogs and then all cats!
+    random.shuffle(trainData)
+    return trainData
+
+def reshapeData(trainData, trainX, trainY):
+    #Bulid the X and Y list...
+    for features, label in trainData:
+        trainX.append(features)
+        trainY.append(label)
+
+    #trainX has to be a numpy array - and shape has to be specified
+    trainX = np.array(trainX).reshape(-1, imgSize, imgSize, 1)
+    return (trainX,trainY)
+
+#Save trainX and trainY into files...
+def saveData(trainX, trainY): 
+    pickleOut = open("X.pickle", "wb")
+    pickle.dump(trainX, pickleOut)
+    pickleOut.close()
+
+    pickleOut = open("Y.pickle", "wb")
+    pickle.dump(trainY, pickleOut)
+    pickleOut.close()
+
+#Load saved python serialized objects...
+def loadData():
+    trainX = pickle.load(open("X.pickle", "rb"))
+    trainY = pickle.load(open("Y.pickle", "rb"))
+    return (trainX, trainY)
+
+#Building the model (CNN)
+def bulidCNNModel(trainX):
+    model = Sequential()
+
+    model.add(Conv2D(64, (3,3), input_shape = trainX.shape[1:]))
+    model.add(Activation("relu"))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+
+    model.add(Conv2D(64, (3,3)))
+    model.add(Activation("relu"))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+
+    model.add(Flatten())
+    model.add(Dense(64))
+
+    model.add(Dense(1))
+    model.add(Activation("sigmoid"))
+
+    return model
+
+#Compile the model
+def compileModel(model):
+    model.compile(loss="binary_crossentropy",
+                optimizer="adam",
+                metrics=["accuracy"])
+    return model
+
+#Train...
+def fitModel(model, trainX, trainY):    
+    model.fit(trainX, trainY, batch_size=32, epochs=10, validation_split=0.1)
+    return model
+
+def main():
+    trainData = []
+    trainData = createTrainData(trainData)
+    trainX = []
+    trainY = []
+    (trainX, trainY) = reshapeData(trainData, trainX, trainY)
+    saveData(trainX, trainY)
+
+    #Normalize the data (images again...)
+    trainX = trainX / 255.0
+
+    model = bulidCNNModel(trainX)
+    model = compileModel(model)
+    model = fitModel(model, trainX, trainY)
+
+if __name__ == "__main__":
+    main()
